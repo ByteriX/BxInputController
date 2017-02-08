726a09f58d431cc5e9cd01d9e4f2d5e8ee215a03
Sergey Balalaev <sof.bix@gmail.com>
2017-02-08 21:46:15 +0400

    fix uniqual

--- BxInputController/Sources/Cells/BxInputSelectorPicturesLibraryDataSource.swift
+++ BxInputController/Sources/Cells/BxInputSelectorPicturesLibraryDataSource.swift
@@ -75,14 +75,16 @@ open class BxInputSelectorPicturesLibraryDataSource : NSObject {
         {
             return false
         }
-        let _ = data.pictures.index(where: { (currentPicture) -> Bool in
+        if let _ = data.pictures.index(where: { (currentPicture) -> Bool in
             if let currentPicture = currentPicture as? BxInputPictureLibraryItem {
                 return currentPicture == picture
             } else {
                 return false
             }
             
-        })
+        }) {
+            return true
+        }
         return false
     }
     
